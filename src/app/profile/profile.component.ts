7b246e2c53f068cfee40853a3d9ed06b04629cf0
Yann Mulonda <xst-mulo7954@stthomas.edu>
2020-01-16 17:15:34 -0600

    setup user logout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +10,15 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class ProfileComponent implements OnInit {
 
     constructor(
-        private afAuth: AngularFireAuth
+        private afAuth: AngularFireAuth,
+        private authservice: AuthService
     ) { }
 
     ngOnInit() {
+    }
+
+    logout() {
+      this.authservice.logout();
     }
 
 }
